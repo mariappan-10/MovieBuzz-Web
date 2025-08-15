a609c702b80a4156e5c62cc285107962acf55529
Mariappan <mahesh1042000@gmail.com>
2025-08-15 18:14:46 +0530

    added year and page filter

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:5231/api';
-// export const API_BASE_URL = 'https://movie-buzz.azurewebsites.net/api/v1';
+//export const API_BASE_URL = 'http://localhost:5231/api';
+export const API_BASE_URL = 'https://movie-buzz.azurewebsites.net/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
